e48369813fdbb4091c42797b8c5178cadd34f96d
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-10-14 16:57:36 +0900

    Add: either.io backend server

---
+++ vs/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'vs'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('lucky/', views.lucky, name='lucky'),
+    path('<int:id>/detail/', views.detail, name='detail'),
+
+    path('create/', views.create, name='create'),
+    path('<int:id>/choice/<int:select>/', views.choice, name='choice'),
+
+    path('<int:id>/update/', views.update, name='update'),
+
+    path('<int:id>/delete/', views.delete, name='delete'),
+    path('<int:q_id>/<int:c_id>/choice-delete/', views.choice_delete, name='choice-delete'),
+]
---
+++ vs/views.py
@@ -0,0 +1,143 @@
+from django.shortcuts import render, redirect
+from .models import Question, Choice
+import random
+
+# Create your views here.
+
+def caculate(arr, id):
+
+    p1 = 0
+    p2 = 0
+    result = []
+    
+    for a in arr:
+        if a.question_id == id:
+            result.append(a)
+            if a.pick == 1:
+                p1 += 1
+            else:
+                p2 += 1
+
+    if p1 + p2 != 0:
+
+        p1_per = round(p1 * 100 / (p1 + p2), 2)
+        p2_per = round(p2 * 100 / (p1 + p2), 2)
+
+    else:
+
+        p1_per = 0
+        p2_per = 0
+    
+    return (p1, p2, p1_per, p2_per, result)
+
+def index(request):
+
+    question = Question.objects.all()
+
+    context = {
+        'question' : question,
+    }
+
+    return render(request, 'index.html', context)
+
+def lucky(request):
+
+    total = Question.objects.all()
+
+    q_id = random.randrange(0, len(total))
+    question = total[q_id]
+
+    choices = Choice.objects.all()
+
+    p1, p2, p1_per, p2_per, result = caculate(choices, question.id)
+
+    context = {
+        'question':question,
+        'p1' : p1,
+        'p2' : p2,
+        'p1_per' : p1_per,
+        'p2_per' : p2_per,
+        'result' : result,
+    }
+
+    return render(request, 'lucky.html', context)
+
+
+def create(request):
+
+    if request.method == 'POST':
+
+        question = request.POST.get('question')
+        answer1 = request.POST.get('answer1')
+        answer2 = request.POST.get('answer2')
+
+        Question.objects.create(question=question, answer1=answer1, answer2=answer2)
+
+        return redirect('vs:index')
+
+    else:
+
+        return render(request, 'create.html')
+
+def update(request, id):
+
+    question = Question.objects.get(id=id)
+
+    if request.method == 'POST':
+
+        question.question = request.POST.get('question')
+        question.answer1 = request.POST.get('answer1')
+        question.answer2 = request.POST.get('answer2')
+
+        question.save()
+
+        return redirect('vs:index')
+
+    else:
+
+        context = {
+            'question':question,
+        }
+
+        return render(request, 'update.html', context)
+
+def delete(request, id):
+
+    question = Question.objects.get(id=id)
+    question.delete()
+
+    return redirect('vs:index')
+
+def choice(request, id, select):
+
+    question = Question.objects.get(id=id)
+
+    Choice.objects.create(pick=select, comment='None', question=question)
+
+    return redirect('vs:detail', id)
+
+def detail(request, id):
+
+    question = Question.objects.get(id=id)
+
+    choices = Choice.objects.all()
+
+    p1, p2, p1_per, p2_per, result = caculate(choices, id)
+
+    context = {
+        'question':question,
+        'p1' : p1,
+        'p2' : p2,
+        'p1_per' : p1_per,
+        'p2_per' : p2_per,
+        'result' : result,
+        'choices': choices,
+    }
+
+    return render(request, 'detail.html', context)
+
+def choice_delete(request, q_id, c_id):
+
+    Choice.objects.get(id=c_id).delete()
+
+    return redirect('vs:detail', q_id)
